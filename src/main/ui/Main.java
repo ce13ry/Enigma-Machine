9ba03cdd2672f736a1cf4d50535b7c60becbb75c
Caleb <cz1025@students.cs.ubc.ca>
2025-02-05 11:27:41 -0800

    README update

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,14 +3,13 @@ import model.Enigma;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Enigma socialSecurtiyNum = new Enigma();
+        Enigma brenden = new Enigma();
 
-        socialSecurtiyNum.addSetting4();
-        socialSecurtiyNum.addSetting1();
-        socialSecurtiyNum.addSetting3();
-        socialSecurtiyNum.addSetting3();
+        brenden.addSetting1();
+        brenden.addSetting3();
+        brenden.addSetting2();
 
-        System.out.println(socialSecurtiyNum.cipher("my social security number is 1234567890."));
-        System.out.println(socialSecurtiyNum.cipher("3g49buhs1,xnbp1b4faixh6j.fyv8ptkw8,q3n37"));
+        System.out.println(brenden.cipher("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+        System.out.println(brenden.cipher("k88jb,24vw2g3unb,6qjfce.i78 3"));
     }
 }
